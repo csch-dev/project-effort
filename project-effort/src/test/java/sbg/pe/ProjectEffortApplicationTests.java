ae1754b22d66c52d627c60b7390212cef368ba8c
Carla <carla.schreiber@iav.de>
2026-03-20 09:31:29 +0100

    Projektrahmen erzeugt

---
+++ project-effort/src/test/java/sbg/pe/ProjectEffortApplicationTests.java
@@ -0,0 +1,13 @@
+package sbg.pe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEffortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
